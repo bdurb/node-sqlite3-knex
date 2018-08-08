71e0e151dcccee7417f81697f27941f13a6d8bc2
bdurb <brian@rpmgreenville.com>
2018-08-07 20:21:05 -0700

    added seed file for users table

--- migrations/20180807201224_users_table.js
+++ migrations/20180807201224_users_table.js
@@ -1,6 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('users', function(tbl) {
+    tbl.increments()
+
+    tbl.string('name')
+    .notNullable()
+    .unique()
+    .timestamps()
+  })
 };
 
 exports.down = function(knex, Promise) {
