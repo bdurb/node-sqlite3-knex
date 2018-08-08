08552a2e57c650f42e05df1f248c32f34cc53b84
bdurb <brian@rpmgreenville.com>
2018-08-07 20:51:21 -0700

    set up knex for config

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const db = require('./data/posts.sqlite3');
 
 const server = express();
 server.use(express.json());
 
 const PORT = 3300;
 
-
+server.get('users', (req, res) => {
+  db('users')
+  .then(users => {
+    res.status(200).json(users);
+  })
+  .catch(() => {
+    res.status(500).json({ message: "error, error, error"})
+  })
+})
 
 
 
